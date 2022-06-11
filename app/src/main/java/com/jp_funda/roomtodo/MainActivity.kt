794c714ef2934186844ee685e4213d8b5b111761
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2022-06-11 22:49:49 +0900

    Add fab

--- app/src/main/java/com/jp_funda/roomtodo/MainActivity.kt
+++ app/src/main/java/com/jp_funda/roomtodo/MainActivity.kt
@@ -5,12 +5,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Surface
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.viewmodel.compose.LocalViewModelStoreOwner
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -43,7 +44,12 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainContent(viewModel: MainViewModel) {
-    Scaffold(floatingActionButton = {}) {
+    Scaffold(floatingActionButton = {
+        FloatingActionButton(onClick = { /*TODO*/ }) {
+            Icon(imageVector = Icons.Default.Add, contentDescription = "追加ボタン")
+        }
+    }) {
 
     }
 }
+
