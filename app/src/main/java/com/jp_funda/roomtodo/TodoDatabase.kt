c89f2978e64fa13876a5cf6b64248f329a8f030b
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2022-06-11 15:31:33 +0900

    Add helper function to get database instance

--- app/src/main/java/com/jp_funda/roomtodo/TodoDatabase.kt
+++ app/src/main/java/com/jp_funda/roomtodo/TodoDatabase.kt
@@ -1,9 +1,33 @@
 package com.jp_funda.roomtodo
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(entities = [Todo::class], version = 1)
 abstract class TodoDatabase: RoomDatabase() {
     abstract fun todoDao(): TodoDao
+
+    companion object {
+        private var INSTANCE: TodoDatabase? = null
+
+        fun getInstance(context: Context): TodoDatabase {
+            synchronized(this) {
+                var instance = INSTANCE
+
+                if (instance == null) {
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        TodoDatabase::class.java,
+                        "todo_database"
+                    ).fallbackToDestructiveMigration()
+                        .build()
+
+                    INSTANCE = instance
+                }
+                return instance
+            }
+        }
+    }
 }
