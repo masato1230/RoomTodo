f8bf4b1daf562f8af034ef1b0ef4414bd438d3a1
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2022-06-11 13:21:35 +0900

    Add todo fields

--- app/src/main/java/com/jp_funda/roomtodo/Todo.kt
+++ app/src/main/java/com/jp_funda/roomtodo/Todo.kt
@@ -1,4 +1,11 @@
 package com.jp_funda.roomtodo
 
-class Todo {
-}
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Todo(
+    @PrimaryKey(autoGenerate = true) val id: Int,
+    val title: String,
+    val description: String,
+)
